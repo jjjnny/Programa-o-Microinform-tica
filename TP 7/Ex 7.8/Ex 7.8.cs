6c601d354378e99be036e57e0839c7604297a80b
Jennyfer Cavalcante Marinho <80689839+jjjnny@users.noreply.github.com>
2022-09-20 09:09:10 -0300

    Add files via upload

---
+++ TP 7/Ex 7.8/Ex 7.8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex_7._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            c = 0;
+            while (c < 10) {
+                c = c + 1;
+                Console.WriteLine(c+", 1 2 3 4 5 6 7 8 9 10");
+            }
+            
+        }
+    }
+}
