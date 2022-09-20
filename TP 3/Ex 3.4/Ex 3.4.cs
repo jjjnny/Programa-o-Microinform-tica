7a1005aa19430323f4b0f7bff10f093abb017a89
Jennyfer Cavalcante Marinho <80689839+jjjnny@users.noreply.github.com>
2022-09-20 08:55:03 -0300

    Add files via upload

---
+++ TP 3/Ex 3.4/Ex 3.4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ex_3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome completo: ");
+            string nome = Convert.ToString(Console.ReadLine());
+            string nomeMI = nome.ToLower();
+            int pos = nomeMI.IndexOf (" ");
+            string email = nomeMI.Substring(0, pos);
+            Console.WriteLine(email+"@fatec.sp.gov.br");
+        }
+    }
+}
---
+++ TP 3/Ex 3.5/Ex 3.5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex_3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome completo: ");
+            string nomeC = Convert.ToString(Console.ReadLine());
+            string nomeCMI = nomeC.ToLower();
+            int pos1 = nomeCMI.IndexOf(" ");
+            int pos2 = nomeCMI.LastIndexOf(" ");
+            string nome = nomeCMI.Substring(0, pos1);
+            string sobn = nomeCMI.Substring(pos2);
+            string sobnE = sobn.Trim();
+            Console.WriteLine(nome+"."+sobnE+"@fatec.sp.gov.br");
+            Console.ReadKey();
+        }
+    }
+}
