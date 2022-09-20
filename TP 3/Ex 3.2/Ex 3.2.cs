3dc25527e075f25916250e929e169371874f3415
Jennyfer Cavalcante Marinho <80689839+jjjnny@users.noreply.github.com>
2022-09-20 08:54:03 -0300

    Add files via upload

---
+++ TP 3/Ex 3.2/Ex 3.2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor de a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o valor de b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o valor de c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+            double x1 = (-b + Math.Sqrt(delta))/(2*a);
+            double x2 = (-b - Math.Sqrt(delta))/(2*a);
+            Console.WriteLine("O valor de x1 é: "+x1);
+            Console.WriteLine("O valor de x2 é: "+x2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP 3/Ex 3.3/Ex 3.3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite seu nome completo: ");
+            string nomeC = Convert.ToString(Console.ReadLine());
+            int pos = nomeC.IndexOf(" ");
+            string nome = nomeC.Substring(0, pos);
+            string sobn = nomeC.Substring(pos);
+            string car = nomeC.Substring (5,10);
+            string troca = nomeC.Replace("a", "o");
+            Console.WriteLine("Seu primeiro nome: "+nome);
+            Console.WriteLine("Seu sobrenome: "+sobn); 
+            Console.WriteLine("Caractere do 5 ao 10: "+car);
+            Console.WriteLine("Trocar os caracteres 'A' por 'O': "+troca);
+            Console.ReadKey();
+        }
+    }
+}
